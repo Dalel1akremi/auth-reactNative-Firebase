8e61210f9b2fa5d126434a50008003d4ae7bfdf9
dalel <dalelakremi2020@gmail.com>
2023-12-21 12:05:49 +0100

    feat: fix `App` page tsx file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './app/screens/Login';
+import List from './app/screens/List';
+import Details from './app/screens/Details';
+import React,{useState,useEffect} from 'react';
+import {FIREBASE_AUTH} from './FirebaseConfig';
+import {User,onAuthStateChanged } from 'firebase/auth';
+const Stack = createNativeStackNavigator();
+const InsideStack = createNativeStackNavigator();
+function InsideLayout(){
+  return(
+    <InsideStack.Navigator >
+    <InsideStack.Screen name="My todos" component={List}  />
+    <InsideStack.Screen name="Details" component={Details}  />
+  </InsideStack.Navigator>
+  )
+}
 export default function App() {
+  const[user,setUser]= useState <User | null>(null);
+ useEffect(()=>{
+  onAuthStateChanged(FIREBASE_AUTH,(user)=>{
+    console.log('user',user);
+    setUser(user); 
+   });
+
+},[]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {user?(
+        <Stack.Screen name="Inside" component={InsideLayout} options={{headerShown:true}} />
+        ):(
+       <Stack.Screen name="login" component={Login} options={{headerShown:false}} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
